962e97e2bf554b0b9d8f36922ab688df7b2fe858
kurianbenoy <kurian.bkk@gmail.com>
2024-04-21 18:31:00 +0530

    add vegam whisper

--- server.py
+++ server.py
@@ -1,15 +1,14 @@
 from typing import Optional
 
-# import modal
 from fastapi import FastAPI, Header
 from fastapi.middleware.cors import CORSMiddleware
 from modal import Image, Stub, asgi_app
 
 GPU_TYPE = "T4"
 
-def download_models():
-    # from transformers import  
+def download_models(): 
     from transformers import WhisperProcessor, WhisperForConditionalGeneration
+    from faster_whisper import WhisperModel
 
     WhisperProcessor.from_pretrained(f"smcproject/Malwhisper-v1-small")
     WhisperForConditionalGeneration.from_pretrained(f"smcproject/Malwhisper-v1-small")
@@ -17,10 +16,13 @@ def download_models():
     WhisperProcessor.from_pretrained("smcproject/Malwhisper-v1-medium")
     WhisperForConditionalGeneration.from_pretrained("smcproject/Malwhisper-v1-medium")
 
+    model_path = "vegam-whisper-medium-ml"
+    model = WhisperModel(model_path, device="cuda", compute_type="float16")
+
 
 web_app = FastAPI()
 stub = Stub("pallakku")
-# image = Image.debian_slim().pip_install_from_requirements("requirements.txt")
+
 image = (
     Image.from_registry("nvidia/cuda:11.8.0-cudnn8-runtime-ubuntu22.04", add_python="3.10")
     .apt_install("git", "ffmpeg")
@@ -55,19 +57,23 @@ async def handle_root(user_agent: Optional[str] = Header(None)):
     return "API is all good and passed the health check."
 
 
-@web_app.get("/")
-async def handle_root(user_agent: Optional[str] = Header(None)):
-    print(f"GET /     - received user_agent={user_agent}")
-    return "API is all good and passed the health check."
+@web_app.get("/vegam-whiser")
+async def vegam_whisper():
+    from faster_whisper import WhisperModel
 
-# @web_app.get("/vegam-whiser")
-# async def vegam_whisper():
-#     from faster_whisper import WhisperModel
+    model_path = "vegam-whisper-medium-ml"
+    model = WhisperModel(model_path, device="cuda", compute_type="float16")
 
-#     model_path = "vegam-whisper-medium-ml"
+    segments, info = model.transcribe(
+                    "resampled.wav",
+                    beam_size=5,
+                    language="ml",
+                )
+    result = []
+    for segment in segments:
+        result.append(segment.text)
 
-#     # Run on GPU with FP16
-#     model = WhisperModel(model_path, device="cuda", compute_type="float16")
+    return {"text": " ".join(result)}
 
 
 
